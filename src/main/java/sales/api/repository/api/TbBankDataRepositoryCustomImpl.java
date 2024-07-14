ebe5e5a222b5c1d7cff56841406bb54c0f5b83d2
whtjdxo <whtjdxo@gmail.com>
2024-07-14 10:26:35 +0900

    여신사 계좌 스크래핑 추가 TCREDIT

--- src/main/java/sales/api/repository/api/TbBankDataRepositoryCustomImpl.java
+++ src/main/java/sales/api/repository/api/TbBankDataRepositoryCustomImpl.java
@@ -16,35 +16,55 @@ public class TbBankDataRepositoryCustomImpl implements TbBankDataRepositoryCusto
     @Override
     public List<Object[]> findMyBankAccountList(String bankCd, String chainNo) {
         String sql = "";
-        sql += "SELECT TC.CHAIN_NO          AS COMP_CD                  ";
-        sql += "        , TC.CHAIN_NAME     AS COMP_NM                  ";
-        sql += "        , TC.ABANK          AS BANK_CD                  ";
-        sql += "        , TC.AACCOUNT       AS ACCOUNT_NO               ";
-        sql += "        , TC.AACCOUNT_PWD   AS ACCOUNT_PWD              ";
-        sql += "        , DECODE(NVL(TC.CORP_NO, 'X'), 'X', 'PERS', 'COMP') AS CORP_TYPE ";
-        sql += "        , TC.CHAIN_BIZ_NO       AS BIZ_NO               ";
-        sql += "        , P.PERSON_REGNO        AS BOSS_REGNO           ";
-        sql += "        , TC.ADEPOSITOR         AS ACCOUNT_NM           ";
-
-        sql += "  FROM  TCHAIN TC                                       ";
-        sql += "        JOIN TPERSON P ON P.PERSON_NO = TC.PERSON_NO    ";
-        sql += " WHERE  TC.SVC_STAT IN ('O', 'R')                       ";
-        sql += "    AND TC.ABANK = '" + bankCd +"'                      " ;
-
+        sql += "SELECT  COMP_CD, COMP_NM, BANK_CD, ACCOUNT_NO, ACCOUNT_PWD, CORP_TYPE           ";
+        sql += "        , BIZ_NO, BOSS_REGNO, ACCOUNT_NM                                        ";
+        sql += "  FROM  (                                                                       ";
+        sql += "        SELECT TC.CHAIN_NO          AS COMP_CD                                  ";
+        sql += "                , TC.CHAIN_NAME     AS COMP_NM                                  ";
+        sql += "                , TC.ABANK          AS BANK_CD                                  ";
+        sql += "                , TC.AACCOUNT       AS ACCOUNT_NO                               ";
+        sql += "                , TC.AACCOUNT_PWD   AS ACCOUNT_PWD                              ";
+        sql += "                , DECODE(NVL(TC.CORP_NO, 'X'), 'X', 'PERS', 'COMP') AS CORP_TYPE";
+        sql += "                , TC.CHAIN_BIZ_NO       AS BIZ_NO                               ";
+        sql += "                , P.PERSON_REGNO        AS BOSS_REGNO                           ";
+        sql += "                , TC.ADEPOSITOR         AS ACCOUNT_NM                           ";
+        sql += "          FROM  TCHAIN TC                                                       ";
+        sql += "                JOIN TPERSON P ON P.PERSON_NO = TC.PERSON_NO                    ";
+        sql += "         WHERE  TC.SVC_STAT IN ('O', 'R')                                       ";
+        sql += "           AND  TC.ABANK = '" + chainNo + "'                                    ";
         if (chainNo != null && !chainNo.isEmpty()) {
-            sql += " AND TC.CHAIN_NO = '" + chainNo + "' ";
+            sql += "          AND   TC.CHAIN_NO = '" + chainNo + "'                             ";
         }
-        sql +=  "   AND  NOT EXISTS(SELECT 1 FROM TSC_SCRAP_LOG SL          ";
-        sql +=  "                    WHERE SL.CHAIN_NO  = TC.CHAIN_NO       ";
-        sql +=  "                      AND SL.SCRAP_GB  = 'BANK'             ";
-        sql +=  "                      AND SL.VAN_CD    = '" + bankCd +"'    ";
-        sql +=  "                      AND SL.LAST_SCRAP_DTTM >= (SYSDATE - INTERVAL '90' MINUTE )";
-        sql +=  "                   )                                       ";
-        sql +=  " ORDER BY TC.CHAIN_NO                  ";
+        sql += "          AND  NOT EXISTS(SELECT 1 FROM TSC_SCRAP_LOG SL                        ";
+        sql += "                WHERE SL.CHAIN_NO  = TC.CHAIN_NO                                ";
+        sql += "                AND SL.SCRAP_GB  = 'BANK'                                       ";
+        sql += "                AND SL.VAN_CD    = '" + chainNo + "'                            ";
+        sql += "                AND SL.LAST_SCRAP_DTTM >= (SYSDATE - INTERVAL '90' MINUTE )     ";
+        sql += "                   )                                                            ";
+        sql += "         UNION  ALL                                                             ";
+        sql += "        SELECT  CREDIT_CD           AS CHAIN_CD                                 ";
+        sql += "                , CREDIT_NAME                                                   ";
+        sql += "                , CRD_BANK          AS BANK_CD                                  ";
+        sql += "                , CRD_ACCOUNT       AS ACCOUNT_NO                               ";
+        sql += "                , CRD_ACCOUNT_PWD   AS ACCOUNT_PWD                              ";
+        sql += "                , 'COMP'            AS CORP_TYPE                                ";
+        sql += "                , CRD_BIZ_NO        AS BIZ_NO                                   ";
+        sql += "                , ''                AS BOSS_REG_NO                              ";
+        sql += "                , OPERATOR_NAME     AS ACCOUNT_NM                               ";
+        sql += "          FROM  TCREDIT CR                                                      ";
+        sql += "         WHERE  DEL = 'O'                                                       ";
+        sql += "           AND  NOT EXISTS(                                                     ";
+        sql += "                    SELECT 1 FROM TSC_SCRAP_LOG SL                              ";
+        sql += "                     WHERE SL.CHAIN_NO  = CR.CREDIT_CD                          ";
+        sql += "                       AND SL.SCRAP_GB  = 'BANK'                                ";
+        sql += "                       AND SL.VAN_CD    = '020'                                 ";
+        sql += "                       AND SL.LAST_SCRAP_DTTM >= (SYSDATE - INTERVAL '90' MINUTE )";
+        sql += "                          )                                                     ";
+        sql += "        ) VT                                                                    ";
+        sql += " ORDER  BY VT.COMP_CD                                                           ";
+
         System.out.println(sql);
         Query query = entityManager.createNativeQuery(sql);
-
-
         return query.getResultList();
 
     }
